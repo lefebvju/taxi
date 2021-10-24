662c640773ad5dfc37a31004f53d448f971129d6
lefebvju <julien.bg07@gmail.com>
2021-10-24 12:05:50 +0200

    delete .h

--- Client.h
+++
@@ -1,9 +0,0 @@
-struct Client{
-    std::string nom;
-    std::string prenom;
-    int tel;
-    Adresse adresse;
-
-    Client(std::string nom, std::string prenom, int tel,Adresse adresse) :nom(nom), prenom(prenom), tel(tel), adresse(adresse){};
-
-};
--- Date.h
+++
@@ -1,6 +0,0 @@
-struct Date{
-    int annees;
-    int mois;
-    int jours;
-    Date(int annees, int mois, int jours):annees(annees), mois(mois), jours(jours){};
-};
