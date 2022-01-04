34dd8ef08e9e1ae54a72d784f63fbd4cdd8a2d08
lefebvju <julien.bg07@gmail.com>
2022-01-04 10:07:58 +0100

    creation de la classe registre

--- Hachage.hpp
+++ Hachage.hpp
@@ -11,3 +11,39 @@ int hachage(string chaine)
 
     return nombreHache;
 }
+
+
+class Registre
+{
+private:
+    Adresse* tab[1000];
+public:
+    Registre();
+    ~Registre();
+    Adresse adresse(string label);
+};
+
+Registre::Registre(/* args */)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        tab[i]=nullptr;
+    }
+    
+}
+
+Registre::~Registre()
+{
+}
+
+Adresse Registre::adresse(string label){
+    string s=label;
+    s.erase(remove( s.begin(),  s.end(), ' '), s.end());
+    int indice=hachage(label);
+    if (tab[indice]==nullptr){
+        Adresse* a=new Adresse(label);
+        tab[indice]=a;
+        return *a;
+    }
+        return *(tab[indice]);
+}
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "Chauffeur.hpp"
 #include "Client.hpp"
 #include "Course.hpp"
+#include "Hachage.hpp"
 #include "calendrier.hpp"
 using namespace std;
 
@@ -11,16 +12,17 @@ int main(){
     Chauffeur t1("Lefebvre","Nicolas","0666285939");
     Chauffeur t2("Stragiotti","Nicolas","0666666666");
     Chauffeur t3("Stragiotti","Alexandra","0666666666");
+    Registre r; 
     Client c;
     //for (int client = 0; client < 10; client++)
     //{
-        c=Client("to_string(client)",string("prénom"),123456789,Adresse(string("rue des potiers 07800 la voulte")));
+        c=Client("to_string(client)",string("prénom"),123456789,r.adresse(string("07800 la voulte")));
     
 
     Course cal;//[11];
     //for (int i = 0; i < 10; i++)
     //{
-        cal=Course(t1,RDV(c,Adresse(string("07800 la voulte")),Date(2021,2,1)),1,Adresse(string(" 07800 saint laurent du pape")),Temps(1,30));
+        cal=Course(t1,RDV(c,r.adresse(string("07800 la voulte")),Date(2021,2,1)),1,r.adresse(string(" 07800 saint laurent du pape")),Temps(1,30));
     //}
     /*for (int i = 0; i < 10; i++)
     {
